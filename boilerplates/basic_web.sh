804f772e24235195634c5244d1b85630fc1b8957
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2019-04-30 16:39:33 +0200

    first commit add bboilerplate

---
+++ boilerplates/basic_web.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#get the name of the folder
+echo 📂  folder name?
+read FOLDER_NAME
+#build folder and files
+mkdir $FOLDER_NAME
+echo 📂  created directory named $FOLDER_NAME  📂
+mkdir $FOLDER_NAME/js
+echo 📂  make $FOLDER_NAME/js  📂
+mkdir $FOLDER_NAME/js/libraries
+echo 📂  make $FOLDER_NAME/js/libraries  📂
+touch $FOLDER_NAME/js/scripts.js
+echo ✏️  created file: $FOLDER_NAME/js/scripts.js
+touch $FOLDER_NAME/index.html
+echo ✏️  created file: $FOLDER_NAME/index.html
+touch $FOLDER_NAME/style.css
+echo ✏️  created file: $FOLDER_NAME/style.css
+#get libraries like jquery
+cd $FOLDER_NAME/js/libraries
+curl https://code.jquery.com/jquery-3.4.0.min.js -O
+echo ✏️  created file: $FOLDER_NAME/js/libraries/jquery-3.4.0.min.js
+cd ../../
+THIS_PATH="$PWD"
+echo built all the folders and files in $THIS_PATH
